38c9935dcc788d53799580b4f737a13df4fed261
Daniel Madridano <8.ddedaniel@gmail.com>
2022-12-10 15:23:01 +0100

    Creacion del menu

--- calculadora.cpp
+++ calculadora.cpp
@@ -8,6 +8,13 @@ int main(){
     float n1, n2, suma=0, resta=0, multiplicacion=0, division=0;
     int opc;
 
+    printf("\tCalculadora basica\n");
+    printf("1. Suma\n");
+    printf("2. Resta\n");
+    printf("3. Multiplicacion\n");
+    printf("4. Division\n\n");
+    printf("Selecciona una opcion: "); scanf("%d", &opc);
+
     getch();
     return 0;
 }
